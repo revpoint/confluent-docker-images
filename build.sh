8af68b6c54cbeae6c9f6d35184cd35c5edecaa7c
Jason Jho <jason@blueapron.com>
2016-02-04 14:13:40 -0500

    Update confluent docker images to use v2 with java 7

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 for SCALA_VERSION in ${SCALA_VERSIONS}; do
     echo "Building confluent-platform-${SCALA_VERSION}"
     cp confluent-platform/Dockerfile confluent-platform/Dockerfile.${SCALA_VERSION}
-    sed -i "s/ENV SCALA_VERSION=.*/ENV SCALA_VERSION=\"${SCALA_VERSION}\"/" confluent-platform/Dockerfile.${SCALA_VERSION}
+    sed -i.bak "s/ENV SCALA_VERSION=.*/ENV SCALA_VERSION=\"${SCALA_VERSION}\"/" confluent-platform/Dockerfile.${SCALA_VERSION}
     TAGS="confluent/platform-${SCALA_VERSION}"
     if [ "x$SCALA_VERSION" = "x$DEFAULT_SCALA_VERSION" ]; then
 	TAGS="$TAGS confluent/platform"
@@ -20,4 +20,3 @@ docker build -t confluent/kafka kafka/
 docker build -t confluent/schema-registry schema-registry/
 docker build -t confluent/rest-proxy rest-proxy/
 docker build -t confluent/tools tools/
-
