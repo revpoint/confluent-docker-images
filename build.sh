36f2369c17ec3d68c44732ba3381dd78f08899ab
Ryan Anguiano <ryan.anguiano@gmail.com>
2016-02-09 14:35:08 -0500

    Update dockerfiles

--- build.sh
+++ build.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-. settings.sh
+set -ex
 
-for SCALA_VERSION in ${SCALA_VERSIONS}; do
-    echo "Building confluent-platform-${SCALA_VERSION}"
-    cp confluent-platform/Dockerfile confluent-platform/Dockerfile.${SCALA_VERSION}
-    sed -i "s/ENV SCALA_VERSION=.*/ENV SCALA_VERSION=\"${SCALA_VERSION}\"/" confluent-platform/Dockerfile.${SCALA_VERSION}
-    TAGS="confluent/platform-${SCALA_VERSION}"
-    if [ "x$SCALA_VERSION" = "x$DEFAULT_SCALA_VERSION" ]; then
-	TAGS="$TAGS confluent/platform"
-    fi
-    for TAG in ${TAGS}; do
-	docker build -t $TAG -f confluent-platform/Dockerfile.${SCALA_VERSION} confluent-platform/
-    done
-done
-
-docker build -t confluent/zookeeper zookeeper/
-docker build -t confluent/kafka kafka/
-docker build -t confluent/schema-registry schema-registry/
-docker build -t confluent/rest-proxy rest-proxy/
-docker build -t confluent/tools tools/
+docker build -t docker.jangl.com/confluent-platform confluent-platform/
+docker build -t docker.jangl.com/zookeeper zookeeper/
+docker build -t docker.jangl.com/kafka kafka/
+docker build -t docker.jangl.com/schema-registry schema-registry/
+docker build -t docker.jangl.com/rest-proxy rest-proxy/
+docker build -t docker.jangl.com/confluent-tools tools/
 
